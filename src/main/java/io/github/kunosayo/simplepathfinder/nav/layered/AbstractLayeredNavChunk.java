94fd3da1f74510bd954471a1f3629ef14cf021db
KunoSayo <KunoSayo@outlook.com>
2026-07-06 16:26:36 +0800

    Fix ce

--- src/main/java/io/github/kunosayo/simplepathfinder/nav/layered/AbstractLayeredNavChunk.java
+++ src/main/java/io/github/kunosayo/simplepathfinder/nav/layered/AbstractLayeredNavChunk.java
@@ -36,7 +36,7 @@ public abstract class AbstractLayeredNavChunk implements ILayeredNavChunk {
             if (ref == null) {
                 return null;
             }
-            return ref;
+            return ref.get();
         }
         return null;
     }
